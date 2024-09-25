39d3e83b2421fd868e7ee570abe13fc41ff8a6f4
André Rosatto <linus1178@gmail.com>
2024-09-25 14:40:50 -0300

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,7 @@ const handleInput = (input: HTMLInputElement, scale: TemperatureScales) => {
   <main>
 		<h1 class="text-center text-4xl text-gray-200 font-bold my-10">Temperature Converter</h1>
 		<section class="m-auto flex flex-col max-w-56 md:flex-row gap-4 justify-center">
-			<Temperature :temperature="temperature" scale="c" @input="handleInput($event.target, 'c')" />
-			<Temperature :temperature="temperature" scale="f" @input="handleInput($event.target, 'f')" />
-			<Temperature :temperature="temperature" scale="k" @input="handleInput($event.target, 'k')" />
-			<Temperature :temperature="temperature" scale="r" @input="handleInput($event.target, 'r')" />
+			<Temperature v-for="scale in <Array<TemperatureScales>>['c', 'f', 'k', 'r'] " :temperature="temperature" :scale="scale" @input="handleInput($event.target, scale)" />
 		</section>
 	</main>
 </template>
