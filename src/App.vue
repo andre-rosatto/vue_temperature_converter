3f199bdb1932b65f52cab6d2587a53af623d87f8
André Rosatto <linus1178@gmail.com>
2024-11-04 09:42:29 -0300

    ui fix: padding

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ const handleInput = (input: HTMLInputElement, scale: TemperatureScales) => {
 </script>
 
 <template>
-  <main>
+  <main class="px-2">
 		<h1 class="text-center text-4xl text-gray-200 font-bold my-10">Temperature Converter</h1>
 		<section class="m-auto flex flex-col max-w-56 md:flex-row gap-4 justify-center">
 			<Temperature v-for="scale in <Array<TemperatureScales>>['c', 'f', 'k', 'r'] " :temperature="temperature" :scale="scale" @input="handleInput($event.target, scale)" />
